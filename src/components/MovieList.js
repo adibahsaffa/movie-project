bafdb4fc5abb5766f4445e34e9242673a3c1f885
Adibah Saffa <adibasaffa98@gmail.com>
2022-12-15 00:12:33 +0800

    movie project

---
+++ src/components/MovieList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const MovieList = (props) => {
+
+    const FavouriteComponent = props.favouriteComponent;
+
+    return (
+        <>
+            {props.movies.map((movie, index) => (
+                <div className='image-container d-flex justify-content-start mb-3'>
+                    <img src={movie.Poster} alt='movie'></img>
+                    <div
+                        className='overlay d-flex align-items-center justify-content-center'
+                        onClick={() => props.handleFavouritesClick()}>
+                        <FavouriteComponent />
+                    </div>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default MovieList;
+
